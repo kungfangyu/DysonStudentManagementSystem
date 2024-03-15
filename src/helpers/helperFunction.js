7138b95fff9f487911e3e6dd3f7cb238a1ff5f6b
Fane <fish19921026@gmail.com>
2024-03-15 17:29:46 +0000

    init: create initial helper functions

---
+++ src/helpers/helperFunction.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: Fangyu Kung
+ * @Date: 2024-03-15 17:28:39
+ * @LastEditors: Do not edit
+ * @LastEditTime: 2024-03-15 17:29:03
+ * @FilePath: /csc8019_team_project_frontend/src/helpers/helperFunction.js
+ */
+// code the helper functions
