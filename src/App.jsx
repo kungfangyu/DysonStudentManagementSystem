48e7960768bcaf00a703171e694d9e383cce2230
Fane <fish19921026@gmail.com>
2024-03-21 13:19:09 +0000

    feat: create forget password page

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@
  * @Author: Fangyu Kung
  * @Date: 2024-03-14 20:59:36
  * @LastEditors: Do not edit
- * @LastEditTime: 2024-03-18 22:57:55
+ * @LastEditTime: 2024-03-21 12:55:30
  * @FilePath: /csc8019_team_project_frontend/src/App.jsx
  */
 
 import React, { Suspense } from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import SignIn from './page/SignIn';
-import UserInfo from './page/UserInfo';
+
+import ForgetPassword from './page/signin/ForgetPassword';
+import SignIn from './page/signin/SignIn';
 import BookingAndAbsence from './page/students/bookingAndAbsence/BookingAndAbsence';
 import ModuleList from './page/students/modules/ModuleList';
 import TimeTable from './page/students/timeTable/TimeTable';
+import UserInfo from './page/UserInfo';
 
 const App = () => (
   <Router>
@@ -24,6 +26,7 @@ const App = () => (
         <Route path="/bookingandabsence" element={<BookingAndAbsence />} />
 
         <Route path="/user" element={<UserInfo />} />
+        <Route path="/forget" element={<ForgetPassword />} />
       </Routes>
     </Suspense>
   </Router>
