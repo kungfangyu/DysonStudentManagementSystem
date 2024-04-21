9ef2c9d336c7cb35b6b4e0159033f1c13290dc3e
FangyuKung <fish19921026@gmail.com>
2024-04-21 16:10:52 +0100

    feat: create student time table and male fake events

--- src/common/StudentTimeTable.jsx
+++ src/common/StudentTimeTable.jsx
@@ -2,7 +2,7 @@
  * @Author: Fangyu Kung
  * @Date: 2024-03-17 19:15:36
  * @LastEditors: Do not edit
- * @LastEditTime: 2024-03-17 20:00:28
+ * @LastEditTime: 2024-04-21 16:10:10
  * @FilePath: /csc8019_team_project_frontend/src/common/StudentTimeTable.jsx
  */
 import moment from 'moment';
@@ -12,6 +12,27 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 const localizer = momentLocalizer(moment);
 const StudentTimeTable = () => {
+  const studentModuleEvents = [
+    {
+      title: 'CSC8011', //Module Name
+      start: new Date('2024-04-22T13:00:00.000Z'),
+      end: new Date('2024-04-22T14:30:00.000Z'),
+      allDay: false,
+    },
+    {
+      title: 'CSC9019',
+      start: new Date('2024-04-24T13:00:00.000Z'),
+      end: new Date('2024-04-24T15:30:00.000Z'),
+      allDay: false,
+    },
+    {
+      title: 'CSC8022',
+      start: new Date('2024-04-25T15:00:00.000Z'),
+      end: new Date('2024-04-25T16:30:00.000Z'),
+      allDay: false,
+    },
+  ];
+
   const eventPropGetter = (event, start, end, isSelected) => {
     let newStyle = {
       backgroundColor: 'lightblue',
@@ -34,7 +55,7 @@ const StudentTimeTable = () => {
     <div className="myCustomHeight">
       <Calendar
         localizer={localizer}
-        // events={ownerData}
+        events={studentModuleEvents}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 500 }}
