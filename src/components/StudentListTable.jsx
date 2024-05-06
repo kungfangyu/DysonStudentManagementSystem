432e6367f800491a993c100b2e300eaa5050ae6f
FangyuKung <fish19921026@gmail.com>
2024-05-06 02:26:21 +0100

    feat: create popupEditTutor

--- src/components/StudentListTable.jsx
+++ src/components/StudentListTable.jsx
@@ -2,11 +2,9 @@
  * @Author: Fangyu Kung
  * @Date: 2024-05-01 13:45:32
  * @LastEditors: Do not edit
- * @LastEditTime: 2024-05-06 02:06:02
+ * @LastEditTime: 2024-05-06 02:20:53
  * @FilePath: /csc8019_team_project_frontend/src/components/StudentListTable.jsx
  */
-import React from 'react';
-// import { useState } from 'react';
 import Chip from '@mui/material/Chip';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
@@ -14,11 +12,22 @@ import TableBody from '@mui/material/TableBody';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import React, { useState } from 'react';
 
+import { Button } from '@mui/material';
 import { getStatusColor } from '../helpers/helperFunction';
 import { StyledTableCell, StyledTableRow } from '../style/tableStyle';
+import PopupEditTutor from './PopupEditTutor';
 
 const StudentListTable = ({ studentList }) => {
+  const [popupOpen, setPopupOpen] = useState(false);
+  const handlePopupTutor = () => {
+    setPopupOpen(true);
+  };
+
+  const handlePopupTutorClose = () => {
+    setPopupOpen(false);
+  };
   return (
     <>
       <TableContainer component={Paper}>
@@ -43,7 +52,9 @@ const StudentListTable = ({ studentList }) => {
                 <StyledTableCell align="left">{row.name}</StyledTableCell>
                 <StyledTableCell align="left">{row.email}</StyledTableCell>
                 <StyledTableCell align="left">{row.grade}</StyledTableCell>
-                <StyledTableCell align="left">{row.tutor}</StyledTableCell>
+                <StyledTableCell align="left">
+                  <Button onClick={handlePopupTutor}>{row.tutor}</Button>
+                </StyledTableCell>
 
                 <StyledTableCell align="left">
                   <Chip
@@ -65,12 +76,10 @@ const StudentListTable = ({ studentList }) => {
           </TableBody>
         </Table>
       </TableContainer>
-      {/* <PopupEditStatus
+      <PopupEditTutor
         open={popupOpen}
-        moduleId={selectedModuleId}
-        initStatus={currentStatus} // 使用currentStatus作为初始状态
         handlePopupEditClose={() => setPopupOpen(false)}
-      /> */}
+      />
     </>
   );
 };
